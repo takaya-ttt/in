4c2ed4d170aebd657f5a24d81edf2f6ff34ee1fb
takaya-ttt <137292527+takaya-ttt@users.noreply.github.com>
2023-06-23 11:07:39 +0900

    letの変数宣言 上書きされるものに使用

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 // const、let等の変数宣言
 
-var val1 = "var変数";
-console.log(val1);
+// var val1 = "var変数";
+// console.log(val1);
 
-//var変数は上書き可能
-val1 = "変数を上書き";
-console.log(val1);
+// //var変数は上書き可能
+// val1 = "変数を上書き";
+// console.log(val1);
 
-//var変数は再宣言可能
-var val1 = "変数を再宣言";
-console.log(val1);
+// //var変数は再宣言可能
+// var val1 = "変数を再宣言";
+// console.log(val1);
+
+let val2 = "let変数";
+console.log(val2);
+
+//letは上書きが可能
+val2 = "let変数を上書き";
+console.log(val2);
+
+//letは再宣言す可能
+let val2 = "let変数を再宣言";
