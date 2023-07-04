40528cea7eab01b82c4b4e9be961ee680eb8cd74
takaya-ttt <137292527+takaya-ttt@users.noreply.github.com>
2023-07-04 18:40:12 +0900

    三項演算子

--- src/index.js
+++ src/index.js
@@ -119,3 +119,37 @@
 // */
 // const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん`);
 // sayHello("jack");
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+const nameArr = ["田中", "山田", "ジャック"];
+for (let index = 0; index < nameArr.lenght; index++) {
+  console.log(`${index + 1}番目は${nameArr[index]}です`);
+}
+
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// });
+// console.log(nameArr2);
+
+// nameArr.map((name) => console.log(`${name}です`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 0;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "ジャック") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
